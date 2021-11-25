7f54d4e92558fab40734aa52d50a51a6c3fdded1
jiraphanz <64070015@kmitl.ac.th>
2021-11-25 11:37:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,28 @@
 import discord
 from discord.ext import commands
 from asyncio import sleep 
-bot = commands.Bot(command_prefix="play")
+bot = commands.Bot(command_prefix="!")
 
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Game(''))
+    await bot.change_presence(activity=discord.Game('call !mp3'))
     print("MP3player ready!")
 
 @bot.command()
-async def พี่พีเจ(ctx):
+async def mp3(ctx):
     emBed = discord.Embed(
             title="Mp3player", description="เล่นไฟล์ mp3 จากบอทดิสคอร์ด", color=0x71afe5)
     emBed.set_thumbnail(
             url="https://o.remove.bg/downloads/45b4ff7e-00c0-476d-90b3-0aadab121687/image-removebg-preview.png")
     emBed.add_field(name="นำไฟล์ mp3 ใส่ในโฟลเดอร์",
-                        value='พิมพ์ play ตามด้วยชื่อไฟล์')
+                        value='พิมพ์ !play ตามด้วยชื่อไฟล์')
     emBed.set_footer(text="https://github.com/jiraphanz",
                          icon_url="https://avatars.githubusercontent.com/u/88519295?v=4")
     await ctx.channel.send(embed=emBed)
 
 @bot.command()
-async def เล่น(ctx, message):
+async def play(ctx, message):
     channel = ctx.author.voice.channel
     vc = await channel.connect()
     vc.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg/ffmpeg.exe", 
